e28bba0d7361b3120aa472b8e123400231de083c
InstaGram <shahrianapurbo@gmail.com>
2023-02-19 23:23:46 +0300

    initialCommit

--- flutter_tutorial-1/lib/main.dart
+++ flutter_tutorial-1/lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          backgroundColor: Colors.amber[200],
-          body: Align(
-              alignment: Alignment.topCenter,
-              child: Text('Hey, i am movable now !')))));
+  runApp(
+      // MaterialApp WIdget
+      MaterialApp(
+          debugShowCheckedModeBanner: false,
+          // Scaffold Widget
+          home: Scaffold(
+              backgroundColor: Colors.amber[200],
+              // Align Widget
+              body: Align(
+                  alignment: Alignment.topCenter,
+                  child:
+                      // Text Widget
+                      Text('Hey, i am movable now !')))));
 }
+
+// You can change the align widget with a center widget to make the text in center as well
+/*
+Center Widget
+
+Center(
+  child: Text('I am in the center now :)')
+)
+
+ */
