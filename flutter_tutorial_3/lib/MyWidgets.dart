128392ff007528c2847715edca58f7689c8e0ffc
General Coding <shahrianapurbo@gmail.com>
2023-03-11 21:55:14 +0300

    initialCommit

--- flutter_tutorial_3/lib/MyWidgets.dart
+++ flutter_tutorial_3/lib/MyWidgets.dart
@@ -1,5 +1,6 @@
 // importing flutter package
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 // Creating our own widget to make the screen
 class MyScreen extends StatelessWidget {
@@ -12,6 +13,34 @@ class MyScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
+      body: const Center(
+        child: MyButton(),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // creating a button
+    return ElevatedButton(
+      onPressed: () {
+        // the logic or the work to be done when the button is pressed
+        log('I am being pressed !!');
+        log(' Signed up successfully');
+      },
+
+      // designing the button
+      style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.green,
+          elevation: 0.0,
+          backgroundColor: Colors.black),
+
+      // using the pen icon
+      child: const Icon(Icons.edit),
     );
   }
 }
